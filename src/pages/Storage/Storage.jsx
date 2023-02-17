78793734e834cec80844c5df9d8853642b46878a
Yevhenii <organistiy@gmail.com>
2023-02-17 08:09:01 +0000

    add: snackbar component

--- src/pages/Storage/Storage.jsx
+++ src/pages/Storage/Storage.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import CommonButton from "../../components/common/CommonButton/CommonButton";
+import GridWrapper from "../../components/common/GridWrapper/GridWrapper";
+import BasicSnackbar from "../../components/common/BasicSnackbar/BasicSnackbar";
+
 const Storage = () => {
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+        return;
+        }
+
+    setOpen(false);
+  };
 
     return(
-        <div>Storage page</div>
+        <GridWrapper>
+            <CommonButton variant="contained" onClick={handleClick}>
+                Open success snackbar
+            </CommonButton>
+            <BasicSnackbar
+                open={open}
+                severity='success'
+                onClose={handleClose}
+                message="Success"
+            />
+        </GridWrapper>
     );
 };
 
